a3766cd45c5f42b8e7d0c74b53cd65a197335b82
David Tanzer <business@davidtanzer.net>
2017-11-20 18:19:14 +0100

    Added remaining project skeletons

---
+++ 13-collections-lists/src/test/java/net/davidtanzer/PrimeNumbersTest.java
@@ -0,0 +1,12 @@
+package net.davidtanzer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PrimeNumbersTest {
+	@Test
+	public void doesSomething() {
+		fail("Implement me");
+	}
+}
