69f69e470ff219e9a8e8da6a6b66b12e1dfa4be5
David Tanzer <business@davidtanzer.net>
2017-11-20 17:20:43 +0100

    Added skeleton for 07-generics

---
+++ 07-generics/src/main/java/net/davidtanzer/ListItem.java
@@ -0,0 +1,30 @@
+package net.davidtanzer;
+
+public class ListItem {
+
+	private String content;
+	private ListItem next;
+
+	public ListItem(String content) {
+		this.content = content;
+	}
+
+	public String content() {
+		return content;
+	}
+
+	public ListItem next() {
+		return next;
+	}
+
+	void setNext(ListItem next) {
+		this.next = next;
+	}
+
+	public void insert(String content) {
+		ListItem oldNext = next;
+		next = new ListItem(content);
+		next.setNext(oldNext);
+	}
+
+}
