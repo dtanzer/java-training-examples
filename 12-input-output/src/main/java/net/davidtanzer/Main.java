5460aef7a420a96c678e6d67dbce8db8901265db
David Tanzer <business@davidtanzer.net>
2017-11-22 15:51:47 +0100

    Added sample solution for 12-input-output.txt

--- 12-input-output/src/main/java/net/davidtanzer/Main.java
+++ 12-input-output/src/main/java/net/davidtanzer/Main.java
@@ -1,5 +1,33 @@
 package net.davidtanzer;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+public class Main {
+	public static void main(String[] args) {
+		try(PrintWriter w = new PrintWriter(new FileWriter(new File("test.txt")))) {
+			w.println("Short line");
+			w.println("A longer line. Actually, this line is longer than 20 chars.");
+			w.println("Another short line.");
+		} catch (IOException e) {
+			throw new IllegalStateException(e);
+		}
+		
+		try(BufferedReader r = new BufferedReader(new FileReader(new File("test.txt")))) {
+			String line;
+			while((line=r.readLine()) != null) {
+				System.out.println(line);
+				if(line.length() > 20) {
+					System.err.println("Line is longer than 20 characters!");
+				}
+			}
+		} catch (IOException e) {
+			throw new IllegalStateException(e);
+		}
+	}
 }
