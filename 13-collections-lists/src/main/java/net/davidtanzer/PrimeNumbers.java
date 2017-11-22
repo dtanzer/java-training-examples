e245f1707b3bcf73d5ff93b5b79ba5ea7fdaf062
David Tanzer <business@davidtanzer.net>
2017-11-22 16:02:26 +0100

    Implemented sample solution for 13-collections-lists

--- 13-collections-lists/src/main/java/net/davidtanzer/PrimeNumbers.java
+++ 13-collections-lists/src/main/java/net/davidtanzer/PrimeNumbers.java
@@ -1,5 +1,22 @@
 package net.davidtanzer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrimeNumbers {
+
+	public static List<Integer> generate(int number) {
+		int nextPrimeCandidate = 2;
+		List<Integer> result = new ArrayList<>();
+		
+		while(number > 1) {
+			while(number % nextPrimeCandidate != 0) {
+				nextPrimeCandidate++;
+			}
+			result.add(nextPrimeCandidate);
+			number /= nextPrimeCandidate;
+		}
+		return result;
+	}
 
 }
--- 13-collections-lists/src/test/java/net/davidtanzer/PrimeNumbersTest.java
+++ 13-collections-lists/src/test/java/net/davidtanzer/PrimeNumbersTest.java
@@ -1,12 +1,41 @@
 package net.davidtanzer;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertArrayEquals;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
 
 public class PrimeNumbersTest {
 	@Test
-	public void doesSomething() {
-		fail("Implement me");
+	public void oneHasNoPrimeFactors() {
+		List<Integer> factors = PrimeNumbers.generate(1);
+		assertListEquals(Arrays.asList(), factors);
+	}
+	
+	private void assertListEquals(List<Object> expected, List<Integer> actual) {
+		assertArrayEquals(expected.toArray(), actual.toArray());
+	}
+
+	@Test
+	public void twoHasSinglePrimeFactor() {
+		List<Integer> factors = PrimeNumbers.generate(2);
+		
+		assertListEquals(Arrays.asList(2), factors);
+	}
+
+	@Test
+	public void fourHasTwoPrimeFactors() {
+		List<Integer> factors = PrimeNumbers.generate(4);
+		
+		assertListEquals(Arrays.asList(2, 2), factors);
+	}
+	
+	@Test
+	public void twelveHasThreePrimeFactors() {
+		List<Integer> factors = PrimeNumbers.generate(12);
+		
+		assertListEquals(Arrays.asList(2, 2, 3), factors);
 	}
 }
