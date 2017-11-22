6b15a5fdedd0bc05e98dee63190b22ad14d5cdd9
David Tanzer <business@davidtanzer.net>
2017-11-22 16:31:56 +0100

    Added sample solution for 15-lambdas-simple

--- 15-lambdas-simple/src/main/java/net/davidtanzer/Main.java
+++ 15-lambdas-simple/src/main/java/net/davidtanzer/Main.java
@@ -1,5 +1,29 @@
 package net.davidtanzer;
 
-public class Main {
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
+public class Main {
+	public static void run(Runnable r) {
+		r.run();
+	}
+	public static void consume(Consumer<String> c) {
+		c.accept("Linz");
+	}
+	public static void decide(Predicate<String> p) {
+		String toTest = "Hello World";
+		System.out.println(toTest + " satisfies predicate? " + p.test(toTest));
+	}
+	public static void calc(Function<String, Integer> f) {
+		String toCalc = "Hello World";
+		System.out.println(toCalc + " results in " + f.apply(toCalc));
+	}
+	
+	public static void main(String[] args) {
+		run(() -> System.out.println("Hello World"));
+		consume(toGreet -> System.out.println("Hello "+toGreet));
+		decide(toTest -> toTest.contains("rl"));
+		calc(toCalc -> toCalc.length());
+	}
 }
